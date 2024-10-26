291943d33dc97a62b533b52900a0683a3af6c829
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-10-26 22:53:01 +0530

    Create ws_client.js

---
+++ ws_client.js
@@ -0,0 +1,15 @@
+const WebSocket = require('ws');
+
+const ws = new WebSocket('ws://loccalhost:8080');
+
+ws.onopen = function(){
+    console.log('Connected to server');
+}
+
+ws.onmessage = function(event){
+    console.log('Received message - ', event.data);
+}
+
+ws.onclose = function(){
+    console.log('Connection closed');
+}

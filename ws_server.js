858c3863767ffbaae5d644024773a329023ab1d6
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-10-27 22:28:11 +0530

    Update ws_server.js

--- ws_server.js
+++ ws_server.js
@@ -1,20 +1,19 @@
-const web_socket = require('ws');
+const web_socket = require("ws");
 
-const web_socket_server = new web_socket.Server({ port:8080});
+const web_socket_server = new web_socket.Server({ port: 8080 });
 
 let clients = [];
 
-web_socket_server.on('connection', function connection(ws){
+web_socket_server.on("connection", function connection(ws) {
+  clients.push(ws);
+  console.log("New client connected");
 
-    clients.push(ws);
-    console.log('New client connected');
-
-    ws.on('message', function incoming(message) {
-        console.log("Received message - ", message);
-        clients.forEach(function(client){
-            if(client !== ws){
-                client.send(message);
-            }
-        });
+  ws.on("message", function incoming(message) {
+    console.log("Received message - ", message);
+    clients.forEach(function (client) {
+      if (client !== ws) {
+        client.send(message);
+      }
     });
-})
+  });
+});

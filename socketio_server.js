ba053871551c88d7a0472e77edf57404d6625cd6
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-10-27 22:28:07 +0530

    Create socketio_server.js

---
+++ socketio_server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+server.listen(3000, () => {
+  console.log("server listening at port 3000");
+});
+
+io.on("connection", (socket) => {
+  console.log("new client connected!");
+
+  socket.on("subscribe", (channel) => {
+    console.log(`subscribing to channel: ${channel}`);
+    socket.join(channel);
+  });
+
+  socket.on("unsubscribe", (channel) => {
+    console.log(`leaving channel: ${channel}`);
+    socket.leave(channel);
+  });
+
+  setInterval(() => {
+    io.to("channel1").emit("message", "Hello from channel1!");
+    io.to("channel2").emit("message", "Hello from channel2!");
+  }, 3000);
+});

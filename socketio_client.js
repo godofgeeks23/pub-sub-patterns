476fa21c71914c357ae577212b9146c1647d9c47
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-10-27 22:28:05 +0530

    Create socketio_client.js

---
+++ socketio_client.js
@@ -0,0 +1,11 @@
+const socket = require("socket.io-client")("http://localhost:3000")
+
+socket.on('connect', () => {
+    console.log("connected to web socket server!")
+
+    socket.emit('subscribe', 'channel1');
+
+    socket.on('message', (message) => {
+        console.log(`got message ${message}`)
+    })
+})

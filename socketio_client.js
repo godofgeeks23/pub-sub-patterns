b8273fe6f3619c59c252cf8fc19444e1c6145a1b
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-10-31 23:21:37 +0530

    Update socketio_client.js

--- socketio_client.js
+++ socketio_client.js
@@ -4,7 +4,7 @@ const socket = require("socket.io-client")("http://localhost:3000")
 socket.on('connect', () => {
     console.log("Connected to the WebSocket server!")
 
-    
+    console.log("Subscribing to channel: channel1")
     socket.emit('subscribe', 'channel1');
 
     socket.on('message', (message) => {
